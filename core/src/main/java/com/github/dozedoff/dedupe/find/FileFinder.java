667bddfc29d23f2e89a90c04f8e36ee0f68e4c69
Nicholas Wright <dozedoffagain@gmail.com>
2017-06-11 16:49:54 +0200

    Implemented file finder

---
+++ core/src/main/java/com/github/dozedoff/dedupe/find/FileFinder.java
@@ -0,0 +1,30 @@
+/* The MIT License (MIT)
+ * Copyright (c) 2017 Nicholas Wright
+ * http://opensource.org/licenses/MIT
+ */
+package com.github.dozedoff.dedupe.find;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * Find files in the given paths.
+ * 
+ * @author Nicholas Wright
+ *
+ */
+public class FileFinder {
+	/**
+	 * Recursively find files in the given directory.
+	 * 
+	 * @param directory
+	 *            to search for files
+	 * @return a stream of files in the directory
+	 * @throws IOException if there was an error accessing the file system 
+	 */
+	public Stream<Path> findFiles(Path directory) throws IOException {
+		return Files.walk(directory);
+	}
+}
---
+++ core/src/test/java/com/github/dozedoff/dedupe/find/FileFinderTest.java
@@ -0,0 +1,54 @@
+/* The MIT License (MIT)
+ * Copyright (c) 2017 Nicholas Wright
+ * http://opensource.org/licenses/MIT
+ */
+package com.github.dozedoff.dedupe.find;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.jimfs.Jimfs;
+
+
+public class FileFinderTest {
+	private FileFinder cut;
+	private FileSystem jimfs;
+
+	private Path dirA;
+
+	private Path fileA;
+	private Path fileB;
+
+	@Before
+	public void setUp() throws Exception {
+		jimfs = Jimfs.newFileSystem();
+
+		cut = new FileFinder();
+		setUpFileSystem();
+	}
+
+	private void setUpFileSystem() throws Exception {
+		dirA = jimfs.getPath("dirA");
+		fileA = dirA.resolve("fileA");
+		fileB = dirA.resolve("fileB");
+
+		Files.createDirectory(dirA);
+		Files.createFile(fileA);
+		Files.createFile(fileB);
+	}
+	
+	@Test
+	public void testFindFiles() throws Exception {
+		Stream<Path> files = cut.findFiles(dirA);
+		assertThat(files.collect(Collectors.toList()), hasItems(fileA,fileB));
+	};
+}
