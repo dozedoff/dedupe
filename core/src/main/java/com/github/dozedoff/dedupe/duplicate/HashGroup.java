eacd376037c38912414425f1baef179b9cce1e91
Nicholas Wright <dozedoffagain@gmail.com>
2017-06-12 18:50:54 +0200

    Class to group files by hash

---
+++ core/src/main/java/com/github/dozedoff/dedupe/duplicate/HashGroup.java
@@ -0,0 +1,65 @@
+/* The MIT License (MIT)
+ * Copyright (c) 2017 Nicholas Wright
+ * http://opensource.org/licenses/MIT
+ */
+package com.github.dozedoff.dedupe.duplicate;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import com.github.dozedoff.dedupe.db.table.FileMetaData;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.Multimaps;
+import com.google.common.hash.HashCode;
+
+public class HashGroup {
+	private final Multimap<String, FileMetaData> hashGroups;
+
+	/**
+	 * Create an instance that can group files based on hash.
+	 */
+	public HashGroup() {
+		this.hashGroups = MultimapBuilder.treeKeys().hashSetValues().build();
+	}
+
+	/**
+	 * Add the paths in the stream to the group, based on the file hash of the path.
+	 * 
+	 * @param stream
+	 *            of files to group by hash
+	 */
+	public void add(Stream<FileMetaData> stream) {
+		Multimap<String, FileMetaData> sync = Multimaps.synchronizedMultimap(hashGroups);
+
+		stream.parallel().forEach(new Consumer<FileMetaData>() {
+			@Override
+			public void accept(FileMetaData t) {
+				sync.put(HashCode.fromBytes(t.getHash()).toString(), t);
+			}
+		});
+	}
+
+	/**
+	 * Get the files that have the same hash as at least one other file.
+	 * 
+	 * @return a list of files with at least a other same size file
+	 */
+	public List<FileMetaData> sameHash() {
+		List<FileMetaData> sameHash = new LinkedList<FileMetaData>();
+
+		hashGroups.asMap().values().forEach(new Consumer<Collection<FileMetaData>>() {
+			@Override
+			public void accept(Collection<FileMetaData> t) {
+				if (t.size() > 1) {
+					sameHash.addAll(t);
+				}
+			}
+		});
+		
+		return sameHash;
+	}
+}
---
+++ core/src/test/java/com/github/dozedoff/dedupe/duplicate/HashGroupTest.java
@@ -0,0 +1,49 @@
+/* The MIT License (MIT)
+ * Copyright (c) 2017 Nicholas Wright
+ * http://opensource.org/licenses/MIT
+ */
+package com.github.dozedoff.dedupe.duplicate;
+
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.to;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.awaitility.Duration;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.dozedoff.dedupe.db.table.FileMetaData;
+
+public class HashGroupTest {
+	private static final Duration TIMEOUT = new Duration(2, TimeUnit.SECONDS);
+
+	private HashGroup cut;
+
+	private FileMetaData metaA;
+	private FileMetaData metaB;
+	private FileMetaData metaC;
+
+	@Before
+	public void setUp() throws Exception {
+		cut = new HashGroup();
+
+		byte[] hashA = {0};
+		byte[] hashB = {1};
+		
+		metaA = new FileMetaData("A", 0, 0, hashA.clone());
+		metaB = new FileMetaData("B", 0, 0, hashB.clone());
+		metaC = new FileMetaData("C", 0, 0, hashA.clone());
+
+		cut.add(Arrays.asList(metaA, metaB, metaC).stream());
+	}
+
+	@Test
+	public void testSameHash() throws Exception {
+		FileMetaData[] expected = { metaA, metaC };
+
+		await().atMost(TIMEOUT).untilCall(to(cut).sameHash(), containsInAnyOrder(expected));
+	}
+}
