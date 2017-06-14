6040655e111c23d9d48d843ef9b0eb6f1b4614f2
Nicholas Wright <dozedoffagain@gmail.com>
2017-06-14 22:51:42 +0200

    CLI implementation for hash and compare

--- cli/src/main/java/com/github/dozedoff/dedupe/cli/DedupeCli.java
+++ cli/src/main/java/com/github/dozedoff/dedupe/cli/DedupeCli.java
@@ -7,13 +7,26 @@ package com.github.dozedoff.dedupe.cli;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.Consumer;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.dozedoff.dedupe.db.BatchWriter;
+import com.github.dozedoff.dedupe.db.Database;
+import com.github.dozedoff.dedupe.db.dao.FileMetaDataDao;
+import com.github.dozedoff.dedupe.db.table.FileMetaData;
+import com.github.dozedoff.dedupe.duplicate.HashGroup;
+import com.github.dozedoff.dedupe.duplicate.SizeGroup;
 import com.github.dozedoff.dedupe.file.FileFinder;
+import com.github.dozedoff.dedupe.file.MetaData;
+import com.google.common.base.Stopwatch;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.LruObjectCache;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
@@ -28,29 +41,136 @@ import net.sourceforge.argparse4j.inf.Namespace;
 public class DedupeCli {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DedupeCli.class);
 
-	public static void main(String[] args) {
+	private Namespace ns;
+	private FileMetaDataDao dao;
+	Database database;
+	
+	public static void main(String[] args) throws SQLException {
+		DedupeCli instance = new DedupeCli(args);
+		instance.run();
+	}
 
+	public DedupeCli(String[] args) {
+		ns = parseArgs(args);
+	}
+
+	private Namespace parseArgs(String[] args) {
 		ArgumentParser parser = ArgumentParsers.newArgumentParser("Dedupe CLI").defaultHelp(true)
 				.description("Find duplicate files and replace them with links");
 		parser.addArgument("dir").nargs("*").help("Directories to walk for files");
+		parser.addArgument("-d", "--db").help("Path to the database");
 		
-		Namespace ns = parser.parseArgsOrFail(args);
-		
+		return parser.parseArgsOrFail(args);
+	}
+
+	private void setUpDatabase() throws SQLException {
+		LOGGER.info("Opening database...");
+
+		if (ns.getString("db") == null) {
+			database = new Database();
+		} else {
+			database = new Database(ns.getString("db"));
+		}
+
+		dao = DaoManager.createDao(database.getConnectionSource(), FileMetaData.class);
+		dao.setObjectCache(new LruObjectCache(100));
+
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					database.close();
+				} catch (SQLException e) {
+					LOGGER.error("Failed to close database: {}", e.toString());
+				}
+			}
+		}));
+	}
+
+	private List<Path> findFiles() {
 		FileFinder ff = new FileFinder();
-		
-		
-		for(String path : ns.<String> getList("dir")) {
+
+		MetaData metaData = new MetaData();
+		SizeGroup sizeGroup = new SizeGroup(metaData);
+
+		Stopwatch sw = Stopwatch.createStarted();
+
+		for (String path : ns.<String> getList("dir")) {
 			try {
 				Stream<Path> stream = ff.findFiles(Paths.get(path));
-				stream.forEach(new Consumer<Path>() {
-					@Override
-					public void accept(Path t) {
-						LOGGER.info(t.toString());
-					}
-				});
+				sizeGroup.add(stream);
 			} catch (IOException e) {
 				LOGGER.error("Failed to find files: {}", e.toString());
 			}
 		}
+
+		List<Path> sizeBasedCandidates = sizeGroup.sameSizeFiles();
+
+		LOGGER.info("Found {} files with non-unique file sizes in {}", sizeBasedCandidates.size(), sw.toString());
+
+		return sizeBasedCandidates;
+	}
+
+	public void run() throws SQLException {
+		setUpDatabase();
+		
+		List<Path> sizeBasedCandidates = findFiles();
+
+		LOGGER.info("Building list of known paths...");
+		
+		MetaData metaData = new MetaData();
+
+		LOGGER.info("Generating metadata for candidates...");
+		Stopwatch metadataSW = Stopwatch.createStarted();
+		HashGroup hashGroup = new HashGroup();
+
+		AtomicInteger existingMeta = new AtomicInteger();
+		AtomicInteger newMeta = new AtomicInteger();
+		AtomicInteger totalFiles = new AtomicInteger();
+
+		BatchWriter<FileMetaDataDao, FileMetaData> batchWriter = new BatchWriter<FileMetaDataDao, FileMetaData>(dao);
+
+		hashGroup.add(sizeBasedCandidates.parallelStream().map(new Function<Path, FileMetaData>() {
+			@Override
+			public FileMetaData apply(Path t) {
+				FileMetaData meta = null;
+
+				totalFiles.getAndIncrement();
+
+				try {
+					if (dao.hasMetaData(t)) {
+						existingMeta.getAndIncrement();
+						meta = dao.getMetaDataForPath(t);
+					} else {
+						newMeta.getAndIncrement();
+						meta = metaData.createMetaDataFromFile(t);
+						
+						batchWriter.add(meta);
+					}
+
+					return meta;
+				} catch (IOException e) {
+					LOGGER.warn("Failed to generate metadata for {}: {}", t, e.toString());
+				} catch (SQLException e) {
+					LOGGER.warn("Failed to access database: {} cause: {}", e.toString(),
+							e.getCause() == null ? "null" : e.getCause().toString());
+				}
+
+				return new FileMetaData();
+			}
+		}));
+
+		LOGGER.info(
+				"From a total of {} files, {} files were already known, {} new metadata entries were added and {} errors were encountered",
+				totalFiles, existingMeta, newMeta,
+				totalFiles.get() - newMeta.get() - existingMeta.get());
+		LOGGER.info("Finished generating metadata for {} files in {}", newMeta, metadataSW);
+
+		batchWriter.shutdown();
+
+		List<FileMetaData> hashBasedCandidates = hashGroup.sameHash();
+
+		LOGGER.info("Found {} files with matching hashes", hashBasedCandidates.size());
+		LOGGER.info("Comparing files by contents...");
 	}
 }
