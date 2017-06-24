51089e4c222c545498b723db51abd1748b1c3b0c
Nicholas Wright <dozedoffagain@gmail.com>
2017-06-24 14:26:20 +0200

    Method for updating metadata

--- core/src/test/java/com/github/dozedoff/dedupe/file/MetaDataTest.java
+++ core/src/test/java/com/github/dozedoff/dedupe/file/MetaDataTest.java
@@ -25,23 +25,38 @@ import com.google.common.jimfs.Jimfs;
 public class MetaDataTest {
 	private static final byte[] TEST_DATA_TEMPLATE = "The quick brown fox jumps over the lazy dog"
 			.getBytes(StandardCharsets.US_ASCII);
+	private static final byte[] TEST_DATA_TEMPLATE2 = "Lorem ipsum"
+			.getBytes(StandardCharsets.US_ASCII);
 	private static final String TEST_DATA_SHA512_HASH = "07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6";
+	private static final String TEST_DATA_SHA512_HASH2 = "e2e9cdde07e34612b5a6a81aa41e065fbc8ba5c6dbfd637314b9f2349263dc4a3037bad914f766075e423b5061538adc9650ca25a318c323d9bef4c8940498a4";
 
 	private MetaData cut;
+
 	private byte[] data;
+	private byte[] data2;
+
 	private FileSystem fs;
 	private Path testFile;
 	private FileTime modTime;
 
+	private FileMetaData meta;
+
 	@Before
 	public void setUp() throws Exception {
-		cut = new MetaData();
+
 
 		fs = Jimfs.newFileSystem();
+
+		cut = new MetaData(fs);
+
 		data = TEST_DATA_TEMPLATE.clone();
+		data2 = TEST_DATA_TEMPLATE2.clone();
+
 		modTime = FileTime.from(1, TimeUnit.SECONDS);
 
 		createTestFile();
+
+		meta = cut.createMetaDataFromFile(testFile);
 	}
 
 	private void createTestFile() throws IOException {
@@ -68,9 +83,43 @@ public class MetaDataTest {
 
 	@Test
 	public void testCreateMetaDataFromFile() throws Exception {
-		FileMetaData meta = cut.createMetaDataFromFile(testFile);
-
 		assertThat(meta, is(new FileMetaData(testFile.toString(), TEST_DATA_TEMPLATE.length, modTime.toMillis(),
 				HashCode.fromString(TEST_DATA_SHA512_HASH).asBytes())));
+	}
+
+	@Test
+	public void testIsRegularFile() throws Exception {
+		assertThat(Files.isRegularFile(testFile), is(true));
+	}
+
+	@Test
+	public void testUpdateMetaDataSize() throws Exception {
+		Files.write(testFile, data2);
+
+		cut.updateMetaData(meta);
+
+		assertThat(meta.getSize(), is(Long.valueOf(TEST_DATA_TEMPLATE2.length)));
+
+	}
+
+	@Test
+	public void testUpdateMetaDataModifiedTime() throws Exception {
+		modTime = FileTime.from(2, TimeUnit.SECONDS);
+
+		Files.setLastModifiedTime(testFile, modTime);
+
+		cut.updateMetaData(meta);
+
+		assertThat(meta.getModifiedTime(), is(modTime.toMillis()));
+
+	}
+
+	@Test
+	public void testUpdateMetaDataHash() throws Exception {
+		Files.write(testFile, data2);
+
+		cut.updateMetaData(meta);
+
+		assertThat(meta.getHash(), is(HashCode.fromString(TEST_DATA_SHA512_HASH2).asBytes()));
 	}
 }
